f565788efef96a49d90660a062653909ccfbb6ad
Svetlana Tyshkevich <Lana-14-92@mail.ru>
2021-04-08 18:44:34 +0300

    add id.js

--- pages/users.js
+++ pages/users.js
@@ -1,8 +1,23 @@
+import {useState} from 'react'
+import Link from "next/link";
 
 const Users = () => {
+    const [users, setUsers] = useState([
+        {id: 1, name: 'petya'},
+        {id: 2, name: 'vasya'},
+    ])
     return (
         <div>
             <h1>Список пользователей</h1>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>
+                        <Link href={`/users/${user.id}`}>
+                            <a>{user.name}</a>
+                        </Link>
+                    </li>
+                )}
+            </ul>
         </div>
     );
 };
